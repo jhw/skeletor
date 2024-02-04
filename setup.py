d9b7da964ed3af98f48cecb6609c702dd1b0368b
Justin Worrall <justin.worrall@gmail.com>
2024-02-04 16:02:50 +0000

    bumped to 0.0.5

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def filter_packages(root):
     
 setuptools.setup(
     name="skeletor",
-    version="0.0.4",
+    version="0.0.5",
     author="jhw",
     author_email="justin.worrall@gmail.com",
     description="A test and scaffolding framework for pareto apps",
